520867b53ff2508b7554b6334efea9741643e642
muteroabel081-ux <muteroabel081@gmail.com>
2026-04-16 03:35:33 +0300

    Remove generateToken function from access_token.php

--- api/access_token.php
+++ api/access_token.php
@@ -1,15 +1,1 @@
-<?php
-function generateToken($consumerKey, $consumerSecret) {
-    $url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-    $credentials = base64_encode($consumerKey . ':' . $consumerSecret);
-    
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Basic ' . $credentials]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    
-    $result = json_decode($response, true);
-    return $result['access_token'] ?? null;
-}
-?>
+
